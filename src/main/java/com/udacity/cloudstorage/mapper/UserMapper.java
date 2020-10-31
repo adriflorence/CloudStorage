8213a4fd0ad7118800b8c3c95ec130f50f903cd4
Adri Forczek <forczekadri@gmail.com>
2020-10-31 21:05:15 +0000

    fix: capitalisation in userMapper

--- src/main/java/com/udacity/cloudstorage/mapper/UserMapper.java
+++ src/main/java/com/udacity/cloudstorage/mapper/UserMapper.java
@@ -6,14 +6,14 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface UserMapper {
 
-    @Select("SELECT * FROM USERS WHERE username = #{username}")
-    User getUser(String username); // method names are arbitrary
+    @Select("SELECT * FROM USERS WHERE USERNAME = #{userName}")
+    User getUser(String userName); // method names are arbitrary
 
-    @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) VALUES(#{username}, #{salt}, #{password}, #{firstName}, #{lastName})")
-    @Options(useGeneratedKeys = true)
+    @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) VALUES(#{userName}, #{salt}, #{password}, #{firstName}, #{lastName})")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
     int insert(User user);
 
-    @Update("UPDATE USERS SET USERNAME = #{username}, SALT = #{salt}, PASSWORD = #{password}, FIRSTNAME = #{firstname}, LASTNAME = #{lastname} WHERE USERID = #{userId}")
+    @Update("UPDATE USERS SET USERNAME = #{userName}, SALT = #{salt}, PASSWORD = #{password}, FIRSTNAME = #{firstName}, LASTNAME = #{lastName} WHERE USERID = #{userId}")
     void update(Integer userId);
 
     @Delete("DELETE * FROM USERS WHERE USERID = #{userId}")
