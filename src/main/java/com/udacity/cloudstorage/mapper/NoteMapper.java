54e6ee143a936bfbdac1db906dede0d5729283f4
Adri Forczek <forczekadri@gmail.com>
2020-11-01 16:55:58 +0000

    feature: creates matter for Note model

---
+++ src/main/java/com/udacity/cloudstorage/mapper/NoteMapper.java
@@ -0,0 +1,26 @@
+package com.udacity.cloudstorage.mapper;
+
+import com.udacity.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface NoteMapper {
+
+    @Select("SELECT * FROM NOTES")
+    List<Note> getAllNotes();
+
+    @Select("SELECT * FROM NOTES WHERE userId = #{userId}")
+    List<Note> getNotesByUserId(Integer userId);
+
+    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    int insert(Note note);
+
+    @Update("UPDATE NOTES SET NOTETITLE = #{noteTitle}, NOTEDESCRIPTION = #{noteDescription}")
+    void update(Integer noteId);
+
+    @Delete("DELETE * FROM NOTES WHERE NOTEID = #{noteId}")
+    void delete(Integer noteId);
+}
