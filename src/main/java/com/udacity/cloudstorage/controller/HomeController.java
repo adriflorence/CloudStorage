c1e686070a568c3f5f83f753a0238776f620648b
Adri Forczek <forczekadri@gmail.com>
2020-11-03 20:59:44 +0000

    feature: ContentService to handle File and Credentials creation

--- src/main/java/com/udacity/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/cloudstorage/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.udacity.cloudstorage.controller;
 
+import com.udacity.cloudstorage.model.Credential;
 import com.udacity.cloudstorage.model.Note;
 import com.udacity.cloudstorage.model.User;
 import com.udacity.cloudstorage.service.ContentService;
@@ -10,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class HomeController {
@@ -23,22 +26,35 @@ public class HomeController {
     }
 
     @GetMapping("/home")
-    public String getHomePage(Authentication authentication, @ModelAttribute("newNote") Note note, Model model){
+    public String getHomePage(Authentication authentication, Model model){
         String username = (String) authentication.getPrincipal();
         User user = this.userService.getUser(username);
+
+        model.addAttribute("newNote", new Note());
+        model.addAttribute("newCredentials", new Credential());
         model.addAttribute("notes", this.contentService.getNotesByUserId(user.getUserId()));
         model.addAttribute("files", this.contentService.getFilesByUserId(user.getUserId()));
+        model.addAttribute("credentials", this.contentService.getCredentialsByUserId(user.getUserId()));
         return "home";
     }
 
     @PostMapping("/home/notes")
-    public String saveNote(@ModelAttribute("newNote") Note note, Authentication authentication, Model model){
+    public String saveNote(Authentication authentication, @ModelAttribute("newNote") Note note, Model model){
         String username = (String) authentication.getPrincipal();
         User user = this.userService.getUser(username);
         this.contentService.addNote(user, note);
         note.setNoteDescription("");
         note.setNoteTitle("");
-        model.addAttribute("notes", this.contentService.getNotesByUserId(user.getUserId()));
-        return "home";
+
+        return getHomePage(authentication, model);
+    }
+
+    @PostMapping("/home/files")
+    public String saveFile(Authentication authentication, @RequestParam("fileUpload") MultipartFile multipartFile, Model model){
+        String username = (String) authentication.getPrincipal();
+        User user = this.userService.getUser(username);
+        this.contentService.addFile(user, multipartFile);
+
+        return getHomePage(authentication, model);
     }
 }
