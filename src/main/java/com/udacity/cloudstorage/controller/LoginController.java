20893c31c0d7923f2427649dfe9241e6a01eb5b6
Adri Forczek <forczekadri@gmail.com>
2020-11-03 21:03:52 +0000

    feature: adds authentication to login endpoint

--- src/main/java/com/udacity/cloudstorage/controller/LoginController.java
+++ src/main/java/com/udacity/cloudstorage/controller/LoginController.java
@@ -1,26 +1,33 @@
 package com.udacity.cloudstorage.controller;
 
 import com.udacity.cloudstorage.model.User;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
 
     // @RequestMapping binds the controller to a specific request URL
-    @GetMapping
+    @GetMapping("/login")
     public String getLoginPage(@ModelAttribute("user") User user, Model model){
         return "login";
     }
 
     @PostMapping("/login")
-    public void postCredentials(@ModelAttribute("user") User user, Model model){
-        System.out.println(user.getUserName());
+    public void postCredentials(Authentication authentication, Model model){
+        String username = (String) authentication.getPrincipal();
+        // TODO check credentials
+        model.addAttribute("invalidCredentials", true);
+    }
+
+    @PostMapping("/logout")
+    public String logout(Model model){
+        model.addAttribute("loggedOut", true);
+        return "login";
     }
 
 }
---
+++ src/main/java/com/udacity/cloudstorage/mapper/CredentialMapper.java
@@ -0,0 +1,20 @@
+package com.udacity.cloudstorage.mapper;
+
+import com.udacity.cloudstorage.model.Credential;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface CredentialMapper {
+
+    @Select("SELECT * FROM CREDENTIALS WHERE userId = #{userId}")
+    List<Credential> getCredentialsByUserId(Integer userId);
+
+    @Insert("INSERT INTO CREDENTIALS (url, userName, key, password) VALUES(#{url}, #{userName}, #{key}, #{password}) ")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialId")
+    int insert(Credential credential);
+}
