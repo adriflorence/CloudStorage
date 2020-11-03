e1c37f82508d8b6ffdf20154ec8ce1b3d53f170a
Adri Forczek <forczekadri@gmail.com>
2020-11-03 21:02:21 +0000

    fix: annotation added

--- src/main/java/com/udacity/cloudstorage/mapper/FileMapper.java
+++ src/main/java/com/udacity/cloudstorage/mapper/FileMapper.java
@@ -5,13 +5,14 @@ import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
+@Mapper
 public interface FileMapper {
 
     @Select("SELECT * FROM FILES WHERE userId = #{userId}")
     List<File> getFilesByUserId(Integer userId);
 
-    @Insert("INSERT INTO NOTES (filename, contenttype, filesize, userid, filedata) VALUES(#{fileName}, #{contentType}, #{fileSize}, #{userId}, #{fileData})")
+    @Insert("INSERT INTO FILES (filename, contenttype, filesize, userid, filedata)" +
+            "VALUES(#{fileName}, #{contentType}, #{fileSize}, #{userId}, #{fileData})")
     @Options(useGeneratedKeys = true, keyProperty = "fileId")
     int insert(File file);
-
 }
