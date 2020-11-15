97b339922b17cad9d082a676eec5ea0b2dee925b
Adri Forczek <forczekadri@gmail.com>
2020-11-15 22:06:19 +0000

    feature: deleteFile endpoint

--- src/main/java/com/udacity/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/cloudstorage/controller/HomeController.java
@@ -3,26 +3,29 @@ package com.udacity.cloudstorage.controller;
 import com.udacity.cloudstorage.model.Credential;
 import com.udacity.cloudstorage.model.Note;
 import com.udacity.cloudstorage.model.User;
-import com.udacity.cloudstorage.service.ContentService;
+import com.udacity.cloudstorage.service.CredentialService;
+import com.udacity.cloudstorage.service.FileService;
+import com.udacity.cloudstorage.service.NoteService;
 import com.udacity.cloudstorage.service.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class HomeController {
 
     private UserService userService;
-    private ContentService contentService;
+    private NoteService noteService;
+    private FileService fileService;
+    private CredentialService credentialService;
 
-    public HomeController(UserService userService, ContentService contentService) {
+    public HomeController(UserService userService, NoteService noteService, FileService fileService, CredentialService credentialService) {
         this.userService = userService;
-        this.contentService = contentService;
+        this.noteService = noteService;
+        this.fileService = fileService;
+        this.credentialService = credentialService;
     }
 
     @GetMapping("/home")
@@ -32,28 +35,35 @@ public class HomeController {
 
         model.addAttribute("newNote", new Note());
         model.addAttribute("newCredentials", new Credential());
-        model.addAttribute("notes", this.contentService.getNotesByUserId(user.getUserId()));
-        model.addAttribute("files", this.contentService.getFilesByUserId(user.getUserId()));
-        model.addAttribute("credentials", this.contentService.getCredentialsByUserId(user.getUserId()));
+        model.addAttribute("notes", this.noteService.getNotesByUserId(user.getUserId()));
+        model.addAttribute("files", this.fileService.getFilesByUserId(user.getUserId()));
+        model.addAttribute("credentials", this.credentialService.getCredentialsByUserId(user.getUserId()));
         return "home";
     }
 
-    @PostMapping("/home/notes")
+    @PostMapping("/notes")
     public String saveNote(Authentication authentication, @ModelAttribute("newNote") Note note, Model model){
         String username = (String) authentication.getPrincipal();
         User user = this.userService.getUser(username);
-        this.contentService.addNote(user, note);
+        this.noteService.addNote(user, note);
         note.setNoteDescription("");
         note.setNoteTitle("");
 
         return getHomePage(authentication, model);
     }
 
-    @PostMapping("/home/files")
+    @PostMapping("/files")
     public String saveFile(Authentication authentication, @RequestParam("fileUpload") MultipartFile multipartFile, Model model){
         String username = (String) authentication.getPrincipal();
         User user = this.userService.getUser(username);
-        this.contentService.addFile(user, multipartFile);
+        this.fileService.addFile(user, multipartFile);
+
+        return getHomePage(authentication, model);
+    }
+
+    @PostMapping("/files/delete/{fileId}")
+    public String deleteFile(Authentication authentication, @PathVariable Integer fileId, Model model){
+        fileService.deleteFile(fileId);
 
         return getHomePage(authentication, model);
     }
