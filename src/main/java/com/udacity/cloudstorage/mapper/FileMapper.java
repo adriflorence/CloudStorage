47b3e5958fcfaf421e4eb83dea22bb50c23a4cd9
Adri Forczek <forczekadri@gmail.com>
2020-11-15 21:59:10 +0000

    feature: adds mapping for delete file and getFileById

--- src/main/java/com/udacity/cloudstorage/mapper/FileMapper.java
+++ src/main/java/com/udacity/cloudstorage/mapper/FileMapper.java
@@ -11,8 +11,14 @@ public interface FileMapper {
     @Select("SELECT * FROM FILES WHERE userId = #{userId}")
     List<File> getFilesByUserId(Integer userId);
 
+    @Select("SELECT FROM FILES WHERE fileId = #{fileId}")
+    File getFileById(Integer fileId);
+
     @Insert("INSERT INTO FILES (filename, contenttype, filesize, userid, filedata)" +
             "VALUES(#{fileName}, #{contentType}, #{fileSize}, #{userId}, #{fileData})")
     @Options(useGeneratedKeys = true, keyProperty = "fileId")
     int insert(File file);
+
+    @Delete("DELETE FROM FILES WHERE fileId = #{fileId}")
+    void delete(Integer fileId);
 }
