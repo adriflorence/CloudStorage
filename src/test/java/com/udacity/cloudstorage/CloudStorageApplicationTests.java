8db0c2b88c7d25172df8eacfd1ba4881ba189b0c
Adri Forczek <forczekadri@gmail.com>
2020-11-30 23:53:47 +0000

    test: new user signup

--- src/test/java/com/udacity/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/cloudstorage/CloudStorageApplicationTests.java
@@ -2,6 +2,7 @@ package com.udacity.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,6 +37,38 @@ class CloudStorageApplicationTests {
 	public void getLoginPage() {
 		driver.get("http://localhost:" + this.port + "/login");
 		Assertions.assertEquals("Login", driver.getTitle());
+	}
+
+	@Test
+	public void getSignupPage() {
+		driver.get("http://localhost:" + this.port + "/signup");
+		Assertions.assertEquals("Sign Up", driver.getTitle());
+	}
+
+	/**
+	 * 	Write a test that signs up a new user, logs in,
+	 * 	verifies that the home page is accessible,
+	 * 	logs out, and verifies that the home page is no longer accessible.
+	 */
+	@Test
+	public void newUserSignup() {
+		driver.get("http://localhost:" + this.port + "/signup");
+
+		// Type in first name, last name, user name and password for sign up and submit
+		driver.findElement(By.id("inputFirstName")).sendKeys("a");
+		driver.findElement(By.id("inputLastName")).sendKeys("a");
+		driver.findElement(By.id("inputUserName")).sendKeys("a");
+		driver.findElement(By.id("inputPassword")).sendKeys("a");
+		driver.findElement(By.id("signupSubmit")).click();
+
+		// navigate to login page,  type in credentials and submit
+		driver.get("http://localhost:" + this.port + "/login");
+		driver.findElement(By.id("inputUserName")).sendKeys("a");
+		driver.findElement(By.id("inputPassword")).sendKeys("a");
+		driver.findElement(By.id("submitLogin")).click();
+
+		// check if Home page available
+		Assertions.assertEquals("Home", driver.getTitle());
 	}
 
 }
